16220e2158a565901ec862d762eddce5434ee170
Thauã Magalhães <thauanlucascpl@gmail.com>
2024-08-16 14:45:43 -0300

    docs: main variables added

--- SortingAlgorithms/Visualizer/main.cpp
+++ SortingAlgorithms/Visualizer/main.cpp
@@ -6,19 +6,22 @@
 
 int main()
 {
-    int n = 120;
+    int n = 150;
     int min = 1;
-    int max = 450;
+    int max = 100;
     int seed = 42;
-    clock_t start, end;
+    int width = 1200;
+    int height = 900;
+
+    int fps = 500;
     int *arr = createArray(n, min, max, seed);
 
-    Visualizer visualizer(1200, 900, 500, "Sorting Algorithms Visualizer", min, max, arr, n);
+    Visualizer visualizer(width, height, fps, "Sorting Algorithms Visualizer", min, max, arr, n);
     sf::RenderWindow &window = visualizer.getWindow();
 
     while (window.isOpen())
     {
-        visualizer.run(selectionSort);
+        visualizer.run(insertionSort);
         window.display();
 
         sf::sleep(sf::seconds(5));
--- SortingAlgorithms/sorts.hpp
+++ SortingAlgorithms/sorts.hpp
@@ -3,6 +3,6 @@
 
 void bubleSort(int *arr, int n, ArrayProcessor arrayProcessor, SwapProcessor swapProcessor, CompareProcessor compareProcessor);
 void insertionSort(int *arr, int n, ArrayProcessor arrayProcessor, SwapProcessor swapProcessor, CompareProcessor compareProcessor);
-void mergeSort(int *arr, int n, ArrayProcessor arrayProcessor, SwapProcessor swapProcessor, CompareProcessor compareProcessor);
 void quickSort(int *arr, int n, ArrayProcessor arrayProcessor, SwapProcessor swapProcessor, CompareProcessor compareProcessor);
 void selectionSort(int *arr, int n, ArrayProcessor arrayProcessor, SwapProcessor swapProcessor, CompareProcessor compareProcessor);
+void mergeSort(int *arr, int n, ArrayProcessor arrayProcessor, SwapProcessor swapProcessor, CompareProcessor compareProcessor); // wip
